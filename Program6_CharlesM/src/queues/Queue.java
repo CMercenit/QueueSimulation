d38b4c0b633e35cfa5b9c921dba17c43f8a54815
Charles Mercenit <ccm98@yahoo.com>
2018-04-18 08:09:08 -0400

    queue class done

--- Program6_CharlesM/src/queues/Queue.java
+++ Program6_CharlesM/src/queues/Queue.java
@@ -1,6 +1,33 @@
 package queues;
 
+import java.util.LinkedList;
+
 public class Queue
 {
-
+	private LinkedList<Customer> myLinkedList;
+	
+	public Queue()
+	{
+		myLinkedList = new LinkedList<Customer>();
+	}
+	
+	public void enqueue(Customer customer)
+	{
+		myLinkedList.addFirst(customer);
+	}
+	
+	public Customer dequeue()
+	{
+		return myLinkedList.removeLast();
+	}
+	
+	public boolean isEmpty()
+	{
+		return myLinkedList.isEmpty();
+	}
+	
+	public Customer peek()
+	{
+		return myLinkedList.getLast();
+	}
 }
