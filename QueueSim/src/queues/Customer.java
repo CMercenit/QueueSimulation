c37640d7273d3fbd78211cd2531134169f91b193
CMercenit <ccm98@yahoo.com>
2020-01-14 12:47:11 -0500

    Created runnable .jar as demo for project

--- QueueSim/src/queues/Customer.java
+++ QueueSim/src/queues/Customer.java
@@ -19,16 +19,6 @@ public class Customer
 {
 	private final int MY_NUM_IMAGES = 5;
 	
-//	private final Image REGULAR_PERSON = Toolkit.getDefaultToolkit().getImage("images/RegularMan(Transparent).png");
-//	private final ImageIcon SCALED_REGULAR_PERSON = new ImageIcon(REGULAR_PERSON.getScaledInstance(53, 105, Image.SCALE_SMOOTH));
-//	private final Image BOW_TIE_PERSON = Toolkit.getDefaultToolkit().getImage("images/BowTie(Transparent).png");
-//	private final ImageIcon SCALED_BOW_TIE_PERSON = new ImageIcon(BOW_TIE_PERSON.getScaledInstance(60, 115, Image.SCALE_SMOOTH));
-//	private final Image PONY_TAIL_PERSON = Toolkit.getDefaultToolkit().getImage("images/PonyTail(Transparent).png");
-//	private final ImageIcon SCALED_PONY_TAIL_PERSON = new ImageIcon(PONY_TAIL_PERSON.getScaledInstance(62, 110, Image.SCALE_SMOOTH));
-//	private final Image TOP_HAT_PERSON = Toolkit.getDefaultToolkit().getImage("images/TopHat(Transparent).png");
-//	private final ImageIcon SCALED_TOP_HAT_PERSON = new ImageIcon(TOP_HAT_PERSON.getScaledInstance(53, 118, Image.SCALE_SMOOTH));
-//	private final Image MANAGER_MOM = Toolkit.getDefaultToolkit().getImage("images/ManagerMom(Transparent).png");
-//	private final ImageIcon SCALED_MANAGER_MOM = new ImageIcon(MANAGER_MOM.getScaledInstance(60, 110, Image.SCALE_SMOOTH));
 	private final ImageIcon SCALED_REGULAR_PERSON = new ImageIcon(ResourceLoader
 			.loadImage("RegularMan(Transparent).png")
 			.getScaledInstance(53, 105, Image.SCALE_SMOOTH));
